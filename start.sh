655fb8d94d8b99cb48a8ea022da737dce0ab46bd
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2023-06-23 06:38:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/DeveloperTharun/X-link-bot.git /x_linkz
-cd /x_linkz
+git clone https://github.com/DeveloperTharun/X-link-bot.git /X-link-bot
+cd /X-link-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
